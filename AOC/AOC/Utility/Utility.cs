bd2c409849898c6217ae91b5fc61a92db2686beb
ipperxd <ipperxd@gmail.com>
2024-11-30 12:59:08 +0100

    2023 - Day 1

--- AOC/AOC/Utility/Utility.cs
+++ AOC/AOC/Utility/Utility.cs
@@ -4,7 +4,7 @@ public static class Utility
 {
     public static string[] LoadData(string date)
     {
-        var path = "D:\\Code\\AOC\\AOC\\AOC\\Data\\2023\\" + date;
+        var path = "D:\\Code - Projects\\AOC\\AOC\\AOC\\Data\\2023\\" + date;
         var data = File.ReadAllLines(path);
 
         return data;
