5a531506d6c5ba209aa3b8115077338f1f04deba
Anders Ipsen <ipperxd@gmail.com>
2025-09-13 14:41:27 +0200

    Develop (#1)
    * Add Day 2 solution and develop branch
    
    * Day 2 -2024 and unit test

--- AOC/2024/Day1Solution.cs
+++ AOC/2024/Day1Solution.cs
@@ -1,61 +1,66 @@
 ﻿
-namespace AOC_2024
+namespace AOC_2024;
+public class Day1Solution
 {
-    public class Day1Solution
+    string[] lines = AOC.SharedUtilities.LoadData("2024Day1.txt");
+    int result;
+
+
+    List<int> LeftList = new List<int>();
+    List<int> RighttList = new List<int>();
+
+    public void Start()
     {
-        string[] lines = AOC.SharedUtilities.LoadData("2024Day1.txt");
-
-        int result = 0;
-
-        List<int> LeftList = new List<int>();
-        List<int> RighttList = new List<int>();
-
-        public int SolutionPart1()
+        result = 0;
+        foreach (var line in lines)
         {
-            foreach (var line in lines)
-            {
-                LeftList.Add(int.Parse(line.Substring(0, 5).Trim()));
-                RighttList.Add(int.Parse(line.Substring(6).Trim()));
-            }
+            LeftList.Add(int.Parse(line.Substring(0, 5).Trim()));
+            RighttList.Add(int.Parse(line.Substring(6).Trim()));
+        }
+    }
 
-            LeftList.Sort();
-            RighttList.Sort();
+    public int SolutionPart1()
+    {
+        Start();
+
+        LeftList.Sort();
+        RighttList.Sort();
 
 
-            for (int i = 0; i < LeftList.Count; i++)
-            {
-                var x = (int)MathF.Abs(LeftList[i] - RighttList[i]);
-                result += (int)x;
-            }
-
-            Console.WriteLine($"Day 1 result is: {result}");
-            return result;
+        for (int i = 0; i < LeftList.Count; i++)
+        {
+            var x = (int)MathF.Abs(LeftList[i] - RighttList[i]);
+            result += (int)x;
         }
 
-        public int SolutionPart2()
+        return result;
+    }
+
+    public int SolutionPart2()
+    {
+        Dictionary<int, int> leftRightPairs = new Dictionary<int, int>();
+        
+        Start();
+
+        for (int i = 0; i < RighttList.Count; i++)
         {
-            Dictionary<int, int> leftRightPairs = new Dictionary<int, int>();
-            for (int i = 0; i < RighttList.Count; i++)
+            if (LeftList.Contains(RighttList[i]))
             {
-                if (LeftList.Contains(RighttList[i]))
+                if (!leftRightPairs.ContainsKey(RighttList[i]))
                 {
-                    if (!leftRightPairs.ContainsKey(RighttList[i]))
-                    {
-                        leftRightPairs[RighttList[i]] = 1;
-                    }
-                    else
-                    {
-                        leftRightPairs[RighttList[i]]++;
-                    }
+                    leftRightPairs[RighttList[i]] = 1;
+                }
+                else
+                {
+                    leftRightPairs[RighttList[i]]++;
                 }
             }
-            foreach (var pair in leftRightPairs)
-            {
-                result += pair.Key * pair.Value;
-            }
-
-            Console.WriteLine($"Day 2 result is: {result}");
-            return result;
         }
+        foreach (var pair in leftRightPairs)
+        {
+            result += pair.Key * pair.Value;
+        }
+
+        return result;
     }
 }
--- AOC/Program.cs
+++ AOC/Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AOC;
 using AOC_2024;
 using System.IO;
 
 Day1Solution day = new Day1Solution();
+Day2Solution day2 = new Day2Solution();
 
-Console.WriteLine("Part 1 Reuslt: " + day.SolutionPart1().ToString());
-Console.WriteLine("Part 2 Reuslt: " + day.SolutionPart2().ToString());
+//Console.WriteLine("2024 Day 1 - part 1 Reuslt: " + day.SolutionPart1().ToString()); // Result = 2580760
+//Console.WriteLine("2024 Day 1 - part 2 Reuslt: " + day.SolutionPart2().ToString()); // Result = 25358365
+Console.WriteLine("2024 Day 1 - part 1 Reuslt: " + day2.SolutionPart1(SharedUtilities.LoadData("2024Day2.txt")).ToString()); // Result = 411
+Console.WriteLine("2024 Day 1 - part 2 Reuslt: " + day2.SolutionPart2(SharedUtilities.LoadData("2024Day2.txt")).ToString()); // Result = 465
